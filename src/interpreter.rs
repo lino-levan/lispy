3d322baf4c030547526f93ae48178d91cd73beb3
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-09-28 13:19:09 -0700

    feat: added type coercion to + operator

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -9,16 +9,41 @@ fn evaluate(ast: Ast) -> Ast {
             // evaluate operator
             match operator.as_str() {
                 "+" => {
-                    let mut result = 0.0;
+                    let mut numbered = false;
+                    let mut is_string = false;
+                    let mut num_result = 0.0;
+                    let mut string_result = String::new();
 
                     for operand in operands {
-                        match operand {
-                            Ast::Number(number) => result += number,
-                            _ => panic!("Expected number"),
+                        match is_string {
+                            true => match operand {
+                                Ast::Number(number) => {
+                                    string_result.push_str(number.to_string().as_str())
+                                }
+                                Ast::String(string) => string_result.push_str(string.as_str()),
+                                _ => panic!("Expected number"),
+                            },
+                            false => match operand {
+                                Ast::Number(number) => {
+                                    numbered = true;
+                                    num_result += number
+                                }
+                                Ast::String(string) => {
+                                    is_string = true;
+                                    if numbered {
+                                        string_result.push_str(num_result.to_string().as_str());
+                                    }
+                                    string_result.push_str(string.as_str());
+                                }
+                                _ => panic!("Expected number"),
+                            },
                         }
                     }
 
-                    Ast::Number(result)
+                    match is_string {
+                        true => Ast::String(string_result),
+                        false => Ast::Number(num_result),
+                    }
                 }
                 "print" => {
                     for operand in operands {
